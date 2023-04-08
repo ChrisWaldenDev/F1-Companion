8aacfe5634b35d6e174ca08896cda3ad39da5d7f
Waldxn <chriswalden80@gmail.com>
2023-04-08 19:11:22 -0400

    Added loading functionality to api call

--- client/src/components/ConstructorList/ConstructorList.jsx
+++ client/src/components/ConstructorList/ConstructorList.jsx
@@ -1,14 +1,17 @@
 import {useEffect, useState} from 'react';
 import YearSelectDropdown from "../YearSelectDropdown/YearSelectDropdown.jsx";
 import Header from "../Header/Header.jsx";
+import {CircularProgress} from "@mui/material";
 
 
 function ConstructorList() {
     const [constructorsMap, setConstructorsMap] = useState(new Map());
     const [selectedYear, setSelectedYear] = useState(2023);
+    const [isLoading, setIsLoading] = useState(true);
 
 
     useEffect(() => {
+        setIsLoading(true);
         fetch(`https://ergast.com/api/f1/${selectedYear}/constructorStandings.json`)
             .then(response => response.json())
             .then(data => {
@@ -21,6 +24,7 @@ function ConstructorList() {
                 }, {});
                 const constructorsMap = new Map(Object.entries(constructorsObj));
                 setConstructorsMap(constructorsMap)
+                setIsLoading(false);
             })
     }, [selectedYear])
 
@@ -32,6 +36,7 @@ function ConstructorList() {
     for (let year = currentYear; year >= earliestYear; year--) {
         yearsList.push(year);
     }
+
     function handleYearSelect(value) {
         setSelectedYear(value)
     }
@@ -39,22 +44,34 @@ function ConstructorList() {
     return (
         <>
             <Header/>
-            <h2>List of Constructors</h2>
-            <YearSelectDropdown options={yearsList} onSelect={handleYearSelect} selectedValue={selectedYear}/>
-            <div>
-                <ul>
-                    {[...constructorsMap.values()].map((constructor) => (
-                            <div key={constructor.constructorId}>
-                                <a href={constructor.url}>{constructor.name}</a>
-                                <br/> Position: {constructor.position}
-                                <br/> Points: {constructor.points}
-                                <br/> Wins: {constructor.wins}
-                            </div>
-                    ))}
-                </ul>
-            </div>
-        </>
-    )
+            {isLoading ? (
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh'
+                }}>
+                    <CircularProgress/>
+                </div>
+            ) : (
+                <>
+                    <Header/>
+                    <h2>List of Constructors</h2>
+                    <YearSelectDropdown options={yearsList} onSelect={handleYearSelect} selectedValue={selectedYear}/>
+                    <div>
+                        <ul>
+                            {[...constructorsMap.values()].map((constructor) => (
+                                <div key={constructor.constructorId}>
+                                    <a href={constructor.url}>{constructor.name}</a>
+                                    <br/> Position: {constructor.position}
+                                    <br/> Points: {constructor.points}
+                                    <br/> Wins: {constructor.wins}
+                                </div>
+                            ))}
+                        </ul>
+                    </div>
+                </>)}
+        </>);
 }
 
 export default ConstructorList;
